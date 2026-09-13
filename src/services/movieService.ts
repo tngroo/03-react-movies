c0a958090c596d807b600c95839bc05b38a90759
Vladyslav <vsemencenko15@gmail.com>
2026-09-13 16:02:59 +0300

    movies

---
+++ src/services/movieService.ts
@@ -0,0 +1,28 @@
+import { Movie } from "../types/movie";
+import axios from "axios";
+
+export interface MoviesResponse {
+    page: number;
+    results: Movie[];
+    total_pages: number;
+    total_results: number;
+}
+
+const MY_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export async function fetchMovies(query:string) {
+    
+
+const response = await axios.get(`${BASE_URL}/search/movie` , {
+    params: {
+        query,
+        language: "en-US",
+    }, 
+    headers: {
+    Authorization: `Bearer ${MY_TOKEN}`,
+},
+});
+
+return response.data.results
+}
